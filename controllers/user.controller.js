88d9039ec0a9086e43a843460c4d8efa01290ec3
JustMrArgus <argusprogramming@protonmail.com>
2025-11-04 11:36:43 +0200

    Remade user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,39 +1,66 @@
-const db = require("../data/data");
+const prisma = require("../prisma/client");
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
   const { name } = req.body;
   if (!name) {
     return res.status(400).json({ error: "Name is required" });
   }
-  const newUser = {
-    id: db.userIdCounter++,
-    name,
-  };
-  db.users.push(newUser);
-  res.status(201).json(newUser);
-};
 
-const getUsers = (req, res) => {
-  res.status(200).json(db.users);
-};
-
-const getUser = (req, res) => {
-  const userId = parseInt(req.params.userId, 10);
-  const user = db.users.find((u) => u.id === userId);
-  if (!user) {
-    return res.status(404).json({ error: "User is not found" });
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        account: {
+          create: { balance: 0 },
+        },
+      },
+      include: {
+        account: true,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: "Could not create user" });
   }
-  res.status(200).json(user);
 };
 
-const deleteUser = (req, res) => {
-  const userId = parseInt(req.params.userId, 10);
-  const userIndex = db.users.findIndex((u) => u.id === userId);
-  if (userIndex === -1) {
-    return res.status(404).json({ error: "User is not found" });
+const getUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Could not fetch users" });
   }
-  db.users.splice(userIndex, 1);
-  res.status(204).send();
+};
+
+const getUser = async (req, res) => {
+  const userId = parseInt(req.params.userId, 10);
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) {
+      return res.status(404).json({ error: "User is not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Could not fetch user" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const userId = parseInt(req.params.userId, 10);
+  try {
+    await prisma.user.delete({
+      where: { id: userId },
+    });
+    res.status(204).send();
+  } catch (error) {
+    if (error.code === "P2025") {
+      return res.status(404).json({ error: "User is not found" });
+    }
+    res.status(500).json({ error: "Could not delete user" });
+  }
 };
 
 module.exports = {
